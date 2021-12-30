4974f7a8eedcad6791e70980fa54f828adae1fd4
baki gul <76937950+gulbaki@users.noreply.github.com>
2021-12-30 17:52:35 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+const uri = "mongodb+srv://root:root@cluster0.so5im.mongodb.net/node-auth";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((res) => app.listen(3000))
+.catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.render('home'));
